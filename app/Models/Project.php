276b6cf3f0d74aaf62cb0b2096d940e2234822ae
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-27 17:10:47 +0100

    added functions in controllers

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,9 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['img', 'name', 'slug', 'repository_link', 'description', 'date_start', 'date_end'];
+    protected $fillable = ['img', 'name', 'slug', 'repository_link', 'description', 'date_start', 'date_end', 'type_id'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
